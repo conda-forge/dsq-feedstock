3a3af98597a297b479cec86ffe40e4d29ca6f87c
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 19:16:45 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,9 +1,12 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 go mod edit -replace github.com/mattn/go-ieproxy@v0.0.9=github.com/mattn/go-ieproxy@v0.0.11 || goto :error
 go mod tidy -e || goto :error
 go mod vendor -e || goto :error
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
+go build -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files --ignore github.com/multiprocessio/datastation/runner || goto :error
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
